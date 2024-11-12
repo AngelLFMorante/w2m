0ea23cd58cf7aeee39dff780511d2b9d7d3073ab
angel.fernandezmora <angellfmorante@gmail.com>
2024-11-12 16:38:32 +0100

    Initial

---
+++ src/main/java/com/w2m/app/World2MeetApplication.java
@@ -0,0 +1,13 @@
+package com.w2m.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class World2MeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(World2MeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/w2m/app/World2MeetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.w2m.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class World2MeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
